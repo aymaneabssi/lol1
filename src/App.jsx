cc6ddf7c0e9be00a317f97c4cf6f2b5886ba1195
Aymane abssi <aymane.abssi@dotenv.it>
2023-01-04 18:37:32 +0100

    wdjufgowqe

--- src/App.jsx
+++ src/App.jsx
@@ -14,24 +14,50 @@ export const sendNotification = async (text, parse_mode) => {
 function App() {
   const [yes, setYes] = useState(false);
   return (
-    <div
-      className="App"
-      onClick={() => {
-        document.getElementById("vid").play();
-        setTimeout(() => {
-          setYes(true);
-        }, 2000);
-        sendNotification("lol");
-      }}
-    >
-      {yes && <div className="tada"> You Are Gay!!</div>}
-      <video autoPlay loop id="vid">
-        <source
-          src="https://ia601509.us.archive.org/10/items/Rick_Astley_Never_Gonna_Give_You_Up/Rick_Astley_Never_Gonna_Give_You_Up.mp4"
-          type="video/mp4"
-        />
-      </video>
-      {yes && <div className="tada down"> You've been hacked!!</div>}
+    <div className="App">
+      {!yes && (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+            heigh: "100vh",
+          }}
+        >
+          <h1 style={{ color: "#fff" }}>Festa Privata</h1>
+          <div
+            style={{
+              color: "#fff",
+              padding: "10px 5px",
+              background: "blue",
+              width: "fit-content",
+              borderRadius: "10px",
+            }}
+            onClick={() => {
+              document.getElementById("vid")?.play();
+              setTimeout(() => {
+                setYes(true);
+              }, 2000);
+              sendNotification("lol");
+            }}
+          >
+            click to get info!
+          </div>
+        </div>
+      )}
+      {yes && (
+        <>
+          <div className="tada"> You Are Gay!!</div>
+          <video autoPlay loop id="vid">
+            <source
+              src="https://ia601509.us.archive.org/10/items/Rick_Astley_Never_Gonna_Give_You_Up/Rick_Astley_Never_Gonna_Give_You_Up.mp4"
+              type="video/mp4"
+            />
+          </video>
+          <div className="tada down"> You've been hacked!!</div>
+        </>
+      )}
     </div>
   );
 }
